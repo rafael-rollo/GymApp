716842d1cc92fc144227440608f29cd143aeedbb
Rafael Rollo <rafaelrollo92@gmail.com>
2021-11-11 17:28:28 +0000

    feat(walkthrough): animate the upper shape on walkthrough screen

--- GymApp/Scenes/Walkthrough/WalkthroughViewController.swift
+++ GymApp/Scenes/Walkthrough/WalkthroughViewController.swift
@@ -7,7 +7,25 @@
 
 import UIKit
 
+enum ShapeRotationDegree: CGFloat {
+    case original = -145
+    case firstStep = -130
+    case secondStep = -160
+    case thirdStep = -190
+}
+
 class WalkthroughViewController: UIViewController {
+    
+    private var currentStep: Int = 0
+
+    private lazy var upperShape: UIImageView = {
+        let shape = UIImageView(
+            image: UIImage(named: "AnimatedShape")
+        )
+        shape.translatesAutoresizingMaskIntoConstraints = false
+        shape.transform = CGAffineTransform(rotationAngle: .angle(ShapeRotationDegree.original.rawValue))
+        return shape
+    }()
 
     private lazy var label: UILabel = {
         let label = UILabel()
@@ -23,22 +41,37 @@ class WalkthroughViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        UIView.animate(withDuration: 1.0) {
+            
+            let rad = CGFloat.angle(ShapeRotationDegree.firstStep.rawValue)
+            self.upperShape.transform = CGAffineTransform(rotationAngle: rad)
+        }
     }
 }
 
 extension WalkthroughViewController: ViewCode {
+    
+    func addTheme() {
+        view.backgroundColor = .white
+    }
+    
     func addViews() {
+        view.addSubview(upperShape)
         view.addSubview(label)
     }
 
     func addConstraints() {
+        let upperShapeXOffset = -(upperShape.bounds.width * 0.4)
+        let upperShapeYOffset = -(upperShape.bounds.height * 0.4)
+        NSLayoutConstraint.activate([
+            upperShape.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: upperShapeXOffset),
+            upperShape.topAnchor.constraint(equalTo: view.topAnchor, constant: upperShapeYOffset)
+        ])
+        
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
     }
 
-    func addTheme() {
-        view.backgroundColor = .white
-    }
 }
