45b5b18a83ad87c2d9ee768c9f5bd757e76237e0
Rafael Rollo <rafaelrollo92@gmail.com>
2021-11-12 19:08:37 +0000

    feat(login): add logo and titles for the login screen

--- GymApp/Scenes/Login/LoginViewController.swift
+++ GymApp/Scenes/Login/LoginViewController.swift
@@ -9,12 +9,44 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    private lazy var label: UILabel = {
+    private lazy var logo: UIImageView = {
+        let logo = UIImage(named: "GymAppLogo")?
+            .withTintColor(.terracotta ?? .systemRed)
+
+        let imageView = UIImageView(image: logo)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = .openSans(.bold, size: UIFont.labelFontSize)
-        label.text = "Login Screen!"
+        label.font = .openSans(.bold, size: 20)
+        label.textColor = .shipGray
+        label.text = "Enter your email"
         return label
+    }()
+
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .openSans(size: 14)
+        label.textColor = .secondaryLabel
+        label.text = "It's the one you used to sign up for Gym.app"
+        label.numberOfLines = 2
+        label.lineBreakMode = .byWordWrapping
+        return label
+    }()
+
+    private lazy var titlesView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemBlue.withAlphaComponent(0.1)
+
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
+        return view
     }()
 
     override func loadView() {
@@ -34,13 +66,33 @@ extension LoginViewController: ViewCode {
     }
     
     func addViews() {
-        view.addSubview(label)
+        view.addSubview(logo)
+        view.addSubview(titlesView)
     }
 
     func addConstraints() {
         NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            logo.widthAnchor.constraint(equalToConstant: 120),
+            logo.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 48),
+            logo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+        ])
+
+        NSLayoutConstraint.activate([
+            titlesView.topAnchor.constraint(equalTo: logo.bottomAnchor, constant: 24),
+            titlesView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 24),
+            titlesView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -24)
+        ])
+
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: titlesView.topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: titlesView.leadingAnchor),
+            titleLabel.trailingAnchor.constraint(equalTo: titlesView.trailingAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 12),
+            descriptionLabel.leadingAnchor.constraint(equalTo: titlesView.leadingAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: titlesView.trailingAnchor)
         ])
     }
 
