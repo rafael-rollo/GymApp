8e73e03dfef72094384a76245ead849e858186dc
Rafael Rollo <rafaelrollo92@gmail.com>
2021-11-15 18:21:03 +0000

    feat(home view controller): add dummy home view controller

---
+++ GymApp/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  HomeViewController.swift
+//  GymApp
+//
+//  Created by rafael.rollo on 15/11/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.font = .openSans(.bold, size: 14)
+        label.text = "Home Scene!"
+        return label
+    }()
+
+    override func loadView() {
+        super.loadView()
+        setup()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+extension HomeViewController: ViewCode {
+    
+    func addTheme() {
+        view.backgroundColor = .white
+    }
+    
+    func addViews() {
+        view.addSubview(label)
+    }
+
+    func addConstraints() {
+        label.anchorToCenter(of: view)
+    }
+
+}
