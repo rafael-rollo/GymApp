c3d8b01dca4de0c2d604aad9ae3958435f20ebe1
Rafael Rollo <rafaelrollo92@gmail.com>
2021-11-15 20:14:57 +0000

    fix(expandable header): fix close animation

--- GymApp/Scenes/Home/View/ExpandableHeader.swift
+++ GymApp/Scenes/Home/View/ExpandableHeader.swift
@@ -24,7 +24,7 @@ class ExpandableHeader: UIView {
     // MARK: - subviews
     private lazy var profileImageView: UIImageView = {
         let image = UIImage(named: "ProfilePicture")
-
+        
         let imageView = UIImageView(image: image)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
@@ -34,7 +34,7 @@ class ExpandableHeader: UIView {
         imageView.layer.cornerRadius = 26
         return imageView
     }()
-
+    
     private lazy var nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -43,7 +43,7 @@ class ExpandableHeader: UIView {
         label.text = "Rafael"
         return label
     }()
-
+    
     private lazy var planLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -61,29 +61,29 @@ class ExpandableHeader: UIView {
         label.text = "GymApp ID: 1900916044063"
         return label
     }()
-
+    
     private lazy var profileInfoPanel: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [
             nameLabel, planLabel, gymappIdLabel
         ])
         stack.translatesAutoresizingMaskIntoConstraints = false
-
+        
         stack.axis = .vertical
         stack.distribution = .fill
         stack.alignment = .fill
         return stack
     }()
-
+    
     private var arrowDownImage: UIImage? {
         return UIImage(named: "ArrowDownIcon")?
             .withTintColor(.shipGray ?? .secondaryLabel)
     }
-
+    
     private var arrowUpImage: UIImage? {
         return UIImage(named: "ArrowUpIcon")?
             .withTintColor(.shipGray ?? .secondaryLabel)
     }
-
+    
     private lazy var toggleButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -91,13 +91,13 @@ class ExpandableHeader: UIView {
         button.setContentHuggingPriority(.defaultHigh, for: .horizontal)
         return button
     }()
-
+    
     private lazy var baseContentView: UIStackView = {
         let stack = UIStackView(arrangedSubviews: [
             profileImageView, profileInfoPanel, toggleButton
         ])
         stack.translatesAutoresizingMaskIntoConstraints = false
-
+        
         stack.axis = .horizontal
         stack.distribution = .fill
         stack.alignment = .center
@@ -108,42 +108,45 @@ class ExpandableHeader: UIView {
     private lazy var wrapperView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(baseContentView)
+        view.backgroundColor = .white
         view.layer.zPosition = 100
+        view.layer.masksToBounds = false
+        view.layer.cornerRadius = radius
+        view.addSubview(baseContentView)
         return view
     }()
-
-    private lazy var menu: UITableView = {
+    
+    private lazy var menuTableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.dataSource = self
         tableView.delegate = self
         tableView.register(MenuItemCell.self, forCellReuseIdentifier: MenuItemCell.reuseId)
-        tableView.isHidden = true
         tableView.sectionFooterHeight = .leastNormalMagnitude
         tableView.backgroundColor = .clear
         return tableView
     }()
-
+    
     // MARK: - properties
     override var bounds: CGRect {
         didSet {
             dropShadow()
         }
     }
-
+    
     var heightConstraint: NSLayoutConstraint?
-
+    var menuTopConstraint: NSLayoutConstraint?
+    
     var height: CGFloat {
         let statusBarHeight = UIApplication.shared.windows
             .filter {$0.isKeyWindow}.first?.windowScene?
             .statusBarManager?.statusBarFrame.height ?? 0
-
+        
         return 76 + statusBarHeight
     }
     
     var radius: CGFloat { return 12 }
-
+    
     var isExpanded: Bool = false
     
     var menuItems: [MenuSection] = [
@@ -184,21 +187,21 @@ class ExpandableHeader: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
-
+        
         toggleButton.addTarget(self,
                                action: #selector(toggleButtonPressed(_:)),
                                for: .touchUpInside)
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+        
     override func layoutSubviews() {
         super.layoutSubviews()
-        animatedShadowPath()
+        animateShadowPath()
     }
-
+    
     // MARK: - methods
     private func dropShadow() {
         layer.shadowRadius = 8.0
@@ -206,85 +209,88 @@ class ExpandableHeader: UIView {
         layer.shadowColor = UIColor.secondaryLabel.cgColor
         layer.shadowOffset = .init(width: 0, height: 5)
     }
-
-    private func animatedShadowPath() {
+    
+    private func animateShadowPath() {
         let currentPath = layer.shadowPath
         let newPath = UIBezierPath(roundedRect: bounds, cornerRadius: radius).cgPath
-
+        
         guard let heightAnimation = layer.animation(forKey: "bounds.size")
                 as? CABasicAnimation else {
             layer.shadowPath = newPath
             return
         }
-
+        
         let pathAnimation = CABasicAnimation(keyPath: "shadowPath")
         pathAnimation.duration = heightAnimation.duration
         pathAnimation.timingFunction = heightAnimation.timingFunction
         pathAnimation.fromValue = currentPath
         pathAnimation.toValue = newPath
-
+        
         layer.add(pathAnimation, forKey: "shadowPath")
         layer.shadowPath = newPath
     }
-
+    
     @objc private func toggleButtonPressed(_ sender: UIButton) {
         guard let superview = superview else {
             return
         }
-
+        
         isExpanded = !isExpanded
-
+        
         let toggleImage = isExpanded ? arrowUpImage : arrowDownImage
         toggleButton.setImage(toggleImage, for: .normal)
-        
-        menu.isHidden = !isExpanded
-
+                
         heightConstraint?.constant = isExpanded
             ? superview.bounds.height
             : height
+        
+        menuTopConstraint?.constant = isExpanded
+            ? 0
+            : -UIScreen.main.bounds.height
+        
         UIView.animate(withDuration: 0.5) {
             superview.layoutIfNeeded()
         }
-
-        menu.isHidden = !isExpanded
     }
-    
 }
 
 // MARK: - menu data source
 extension ExpandableHeader: UITableViewDataSource, UITableViewDelegate {
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return menuItems.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return MenuSectionHeader.height
     }
-
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let header = MenuSectionHeader()
         header.title = menuItems[section].title
         return header
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuItems[section].items.count
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return MenuItemCell.height
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: MenuItemCell.reuseId, for: indexPath) as? MenuItemCell else {
             fatalError("Provide an appropriate cell for the menu")
         }
-
+        
         let item = menuItems[indexPath.section].items[indexPath.row]
         cell.setup(from: item)
-
+        
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        debugPrint("section [\(indexPath.section)] - row[\(indexPath.row)]")
     }
     
 }
@@ -301,8 +307,8 @@ extension ExpandableHeader: ViewCode {
     }
     
     func addViews() {
+        addSubview(menuTableView)
         addSubview(wrapperView)
-        addSubview(menu)
     }
     
     func addConstraints() {
@@ -314,12 +320,12 @@ extension ExpandableHeader: ViewCode {
         baseContentView.constrainToTop(of: wrapperView, notchSafe: true)
         baseContentView.constrainHorizontally(to: wrapperView, withMargins: 24)
         baseContentView.constrainHeight(to: 68)
-
+        
         profileImageView.constrainSize(to: .init(width: 52, height: 52))
         toggleButton.constrainSize(to: .init(width: 24, height: 24))
         
-        menu.anchorBelow(of: wrapperView)
-        menu.constrainToBottomAndSides(of: self)
+        menuTopConstraint = menuTableView.anchorBelow(of: wrapperView)
+        menuTableView.constrainToBottomAndSides(of: self)
     }
-
+    
 }
